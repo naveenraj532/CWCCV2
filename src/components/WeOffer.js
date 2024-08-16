7ab5eca3ffd510cd00ffa8563e51f9517057e64d
naveenraj532 <naveenr5a3j2@gmail.com>
2024-08-16 15:10:13 +0530

    services added

--- src/components/WeOffer.js
+++ src/components/WeOffer.js
@@ -1,15 +1,25 @@
-const WeOffer = ({ servicename, paragraph }) => {
-    return (
-      <div className="relative mt-4 group overflow-hidden">
-        <div className="relative z-10 bg-gray-700 h-24 rounded-lg transition-all duration-300 py-2 group-hover:h-36">
-          <h1 className="text-lightgray px-10 py-6 text-xl font-inter font-medium">{servicename}</h1>
+const WeOffer = ({ servicename, paragraph1, paragraph2, Icon, image }) => {
+  return (
+    <div className="relative w-[700px] mt-4 group overflow-hidden">
+      <div className="relative z-10 bg-lightgray text-bold h-24 rounded-lg transition-all duration-300 py-2 group-hover:h-[1270px]">
+        <div className="flex items-center px-10 py-4">
+          <Icon className="text-blue text-4xl" />
+          <h1 className="text-blue text-2xl font-inter font-medium ml-4">
+            {servicename}
+          </h1>
         </div>
-        <p className="text-lightgray px-10 py-6 text-lg font-inter font-medium absolute bottom-0 left-0 right-0 transform translate-y-full opacity-0 transition-transform duration-300 group-hover:translate-y-0 group-hover:opacity-100">
-          {paragraph}
-        </p>
+        <div className="flex flex-col items-center justify-center  transform translate-y-full opacity-0 transition-transform duration-300 group-hover:translate-y-0 group-hover:opacity-100">
+          <img src={image} className="rounded-md w-96 mb-6" />
+          <p className="text-lightblack px-10 text-md font-inter font-medium text-justify mb-2">
+            {paragraph1}
+          </p>
+          <p className="text-lightblack px-10 text-md font-inter font-medium text-justify">
+            {paragraph2}
+          </p>
+        </div>
       </div>
-    );
-  }
-  
-  export default WeOffer;
-  
+    </div>
+  );
+}
+
+export default WeOffer;
